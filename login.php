58306e3d028837d7506119fbba87f07716e35312
dkboss9 <dikeshprajapati13@gmail.com>
2019-05-13 12:29:01 +0545

    Login form with header

--- login.php
+++ login.php
@@ -200,6 +200,11 @@
 	<link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.min.css">
 	<link href="fontawesome/css/all.css" rel="stylesheet">
 	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css ">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
 	<title>Login</title>
 	<style>
 		body{
@@ -229,7 +234,7 @@ span.login {
 	</style>
 </head>
 <body>
-
+<?php include "header.php"; ?>
 	
 
 
